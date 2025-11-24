03b0163dea5a8c0e8dce2daf0a126ec54838cff3
Miguel Angel <68487934+Mundzuk-Uldin@users.noreply.github.com>
2025-11-23 22:54:22 -0500

    Longest Consecutive Sequence with memoization

--- Arrays & Hashing/Longest Consecutive Sequence.py
+++ Arrays & Hashing/Longest Consecutive Sequence.py
@@ -3,32 +3,19 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if(len(nums)< 1):
-            return 0
-        set_list = list(set(nums))
-        longest_list = 0
-        candidate = set_list[0]
-        while(candidate in set_list):
-            long_list = []
-            long_list.append(candidate)
-            del set_list[set_list.index(candidate)]            
-            r = candidate+1
-            l = candidate-1
-            while(l in set_list):
-                long_list.append(l)
-                del set_list[set_list.index(l)]
-                l-=1
-            while(r in set_list):
-                long_list.append(r)
-                del set_list[set_list.index(r)]
-                r+=1
+        longest_length = 0
+        memo = {}
+        for i in range(len(nums)):
+            length = self.nextNumInSequence(nums, nums[i], 0, memo)
+            if(length > longest_length):
+                longest_length = length
                 
-            if(len(long_list)>longest_list):
-                print(long_list)
-                print(longest_list)
-                longest_list = len(long_list)
-                
-            if(len(set_list) > 1):
-                candidate = set_list[0]
-                
-        return longest_list
+        return longest_length
+        
+    def nextNumInSequence(self, nums: List[int], n:int , length: int, memo:dict)-> int:
+        if n not in nums:
+            return length
+        if n in memo:
+            return memo[n]
+        memo[n] = self.nextNumInSequence(nums,n+1,length, memo)
+        return memo[n]
