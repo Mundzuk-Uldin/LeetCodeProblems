6e47a18f4ce71a9439cdcd7a391bb60275a5ee08
Miguel Angel <68487934+Mundzuk-Uldin@users.noreply.github.com>
2025-11-27 13:00:23 -0500

    Two sum in log(n)

--- Arrays & Hashing/Two Sum II.py
+++ Arrays & Hashing/Two Sum II.py
@@ -1,28 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l=0
-        while(l<len(numbers)-1):
-            candidate = numbers[l]
-            pair = target-candidate
-            pair_index = self.binarySearch(numbers, l+1, len(numbers)-1, pair)
-            if(pair_index != -1):
-                return[l+1, pair_index+1]
-            last_element = numbers[l]
-            l += 1
-            while(numbers[l] == last_element):
-                l+=1
-        return []  
-
-    def binarySearch(self, arr, low, high, x):
-        if high >= low:
-            mid = low + (high - low) // 2
-            if arr[mid] == x:
-                return mid
-            elif arr[mid] > x:
-                return self.binarySearch(arr, low, mid-1, x)
+        r= len(numbers)-1
+        while(l<r):
+            combined_num = numbers[l] + numbers[r]
+            if(combined_num == target):
+                return [l+1, r+1]
+            elif(combined_num > target):
+                r-=1
             else:
-                return self.binarySearch(arr, mid + 1, high, x)
-
-        else:
-            return -1
-        
+                l+=1
